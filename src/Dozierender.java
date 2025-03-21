29b292fd5e0034eb6c9bf27c21942d5c3b4c8f5e
BenPeddinghaus <ben.pedding@gmail.com>
2025-03-21 11:18:49 +0100

    Klasse Dozierender ausgefüllt

--- src/Dozierender.java
+++ src/Dozierender.java
@@ -1,3 +1,11 @@
 public class Dozierender {
+    private String name;
+    private char fakultaet;
+    private String bueronummer;
+    
+    public static void frageBeantworten(){
+        System.out.println("Prof. Nestler");
+
+    }
 
 }
