6378d2e52cc2121771423dd4d104d2e353bd2679
LukasChemieBT13 <lukas45.seitle@t-online.de>
2025-03-31 18:40:27 +0200

    test uno

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //test
         
+        Dozierender dozent  = new Dozierender("Dr. Schmidt", "Informatik", "B-123");
+        Lehrveranstaltung swe = new Lehrveranstaltung("Objektorientierte Programmierung", dozent, 5);
+        Studierender student  = new Studierender("Max Mustermann", 123456, "Informatik");
+
     }
 }
