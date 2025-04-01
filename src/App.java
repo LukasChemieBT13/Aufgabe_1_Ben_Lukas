fb0993c22021eba5c7e00209c4c7a8281b887a8b
LukasChemieBT13 <lukas45.seitle@t-online.de>
2025-04-01 11:48:46 +0200

    Methoden weiter getest und verbessert

--- src/App.java
+++ src/App.java
@@ -1,28 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        //test 1
+        //test der Funktionen
+
+        //Objekte erstellen
         Dozierender dozent  = new Dozierender("Prof. Nestler", "Informatik", "B-123");
         Lehrveranstaltung swe = new Lehrveranstaltung("Objektorientierte Programmierung", dozent, 5);
-
-        
         Studierender student  = new Studierender("Max Mustermann", 123456, "Informatik");
         Studierender student2 = new Studierender("Felix Franz",123457, "UXD");
         Studierender student3= new Studierender("Mona Seitz", 123120, "Flug- und fahrzeuginformatik");
 
-
+        //Methoden der Lehrveranstaltung testen
         swe.hinzufuegenStudierenden(student);
         swe.hinzufuegenStudierenden(student2);
-        
         swe.entfernenStudierenden(student2);
-
         swe.anzeigeTeilnehmer();
-
         swe.hinzufügenVorlessungstunde("Liste Arrays", "24.12.2025", "23:10", "G106");
         swe.erzeugePraktikumsstunde("Übung zu OOP", "04.04.2025", "14:00", "E303", "Vorstellung von Git-Aufgabe mit Peddingten");
-        
         swe.anzeigeTeilnehmer();
 
-        //student3.anmeldenfürVeranstaltung(swe);
+        //Methoden der Studierender testen
+        student3.anmeldenfürVeranstaltung(swe);
+        student3.frageStellen(dozent,"Was kommte heute dran?");
+        student3.sprechStundenanfrage("20.04.2025", "B0-13", dozent, "12:30");
+        student3.feedbackGeben(dozent, student3.getName(),"war gut", "24.12.2025");
+
+        //Methoden der Dozierender testen
+        dozent.feedbackErhalten(student3, "war gut", "24.12.2025");
+        dozent.sprechstundeoke("20.04.2025", "B0-13", "12:30");
     }
 }
